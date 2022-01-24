2f0bc7b7652ebbbd8cd9b1a2b8d52d9811b1564e
edson <edsonfilho734@gmail.com>
2022-01-24 16:04:01 -0300

    Role, User

--- backend/src/main/java/com/devsuperior/dslearnbds/entites/Role.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/entites/Role.java
@@ -4,14 +4,19 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
 import java.io.Serializable;
 
 @Data
 @NoArgsConstructor
 @EqualsAndHashCode(exclude = {"authority"})
+@Entity
+@Table(name = "tb_role")
 public class Role implements Serializable {
     private static final long serialVersionId = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String authority;
 
--- backend/src/main/java/com/devsuperior/dslearnbds/entites/User.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/entites/User.java
@@ -1,9 +1,8 @@
 package com.devsuperior.dslearnbds.entites;
 
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
+import javax.persistence.*;
 import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Set;
@@ -11,15 +10,55 @@ import java.util.Set;
 @Data
 @NoArgsConstructor
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
     private static final long serialVersionId = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @EqualsAndHashCode.Include
     private Long id;
     private String name;
+
+    @Column(unique = true)
     private String email;
     private String password;
 
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "tb_user_role",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
     private Set<Role> roles = new HashSet<>();
 
+//    @Override
+//    public Collection<? extends GrantedAuthority> getAuthorities() {
+//        return roles.stream().map(
+//                role -> new SimpleGrantedAuthority(role.getAuthority())).collect(Collectors.toList());
+//    }
+//
+//    @Override
+//    public String getUsername() {
+//        return this.email;
+//    }
+//
+//    @Override
+//    public boolean isAccountNonExpired() {
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean isAccountNonLocked() {
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean isCredentialsNonExpired() {
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean isEnabled() {
+//        return true;
+//    }
 }
