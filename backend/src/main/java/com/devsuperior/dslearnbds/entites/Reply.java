41ffa4e34cdccd07304d6d5faa56a809bb1ca1ff
edson <edsonfilho734@gmail.com>
2023-01-09 11:19:13 -0300

    Criacao do Reply e Topic

---
+++ backend/src/main/java/com/devsuperior/dslearnbds/entites/Reply.java
@@ -0,0 +1,45 @@
+package com.devsuperior.dslearnbds.entites;
+
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.Instant;
+import java.util.HashSet;
+import java.util.Set;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+@Table(name = "tb_reply")
+public class Reply {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Long id;
+
+    @Column(columnDefinition = "TEXT")
+    private String body;
+
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant moment;
+
+    @ManyToOne
+    @JoinColumn(name = "topic_id")
+    private Topic topic;
+
+    @ManyToOne
+    @JoinColumn(name = "author_id")
+    private User author;
+
+    @Setter(AccessLevel.NONE)
+    @ManyToMany
+    @JoinTable(name = "tb_reply_likes",
+            joinColumns = @JoinColumn(name = "reply_id"),
+            inverseJoinColumns = @JoinColumn(name = "user_id")
+    )
+    private Set<User> likes = new HashSet<>();
+
+}
